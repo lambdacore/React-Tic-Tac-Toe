46d64c027e2c1c2da647346c260328582af19823
lambdacore <jonathanwh93@gmail.com>
2024-06-08 13:37:55 -0700

    End of "Lifting State Up" Start of Why immutability is important.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,10 @@
 
 import {useState} from 'react'
 
-function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick () {
-        setValue('X');
-    }
-
+function Square({value, onSquareClick}) {
     return (
         <button
-            className="square"
-            onClick= {handleClick}
+            className="square" onClick={onSquareClick}
         >
             {value}
         </button>
@@ -20,23 +13,31 @@ function Square() {
 }
 
 export default function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick (i) {
+        const nextSquares = squares.slice();
+        nextSquares[i] ="X"
+        setSquares(nextSquares)
+    }
+
     return (
         /* single lines needs ; while fragments do not */
         <>
             <div className="board-row">
-                <Square /> {/* 1 */}
-                <Square /> {/* 2 */}
-                <Square /> {/* 3 */}
+                <Square value = {squares[0]} onSquareClick={() => handleClick(0)} /> {/* 1 */}
+                <Square value = {squares[1]} onSquareClick={ () => handleClick(1)} /> {/* 2 */}
+                <Square value = {squares[2]} onSquareClick={() => handleClick(2)} /> {/* 3 */}
             </div>
             <div className="board-row">
-                <Square /> {/* 4 */}
-                <Square /> {/* 5 */}
-                <Square /> {/* 6 */}
+                <Square value = {squares[3]} onSquareClick={() => handleClick(3)} /> {/* 4 */}
+                <Square value = {squares[4]} onSquareClick={() => handleClick(4)} /> {/* 5 */}
+                <Square value = {squares[5]} onSquareClick={() => handleClick(5)} /> {/* 6 */}
             </div>
             <div className="board-row">
-                <Square /> {/* 7 */}
-                <Square /> {/* 8 */}
-                <Square /> {/* 9 */}
+                <Square value = {squares[6]} onSquareClick={() => handleClick(6)} /> {/* 7 */}
+                <Square value = {squares[7]} onSquareClick={() => handleClick(7)} /> {/* 8 */}
+                <Square value = {squares[8]} onSquareClick={() => handleClick(8)} /> {/* 9 */}
             </div>
         </>
     )
