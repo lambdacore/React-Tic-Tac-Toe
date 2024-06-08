a4d6c552843acf0db8c7327167f28e8ca196bf8d
lambdacore <jonathanwh93@gmail.com>
2024-06-08 14:58:48 -0700

    added satus messages. Now to add time travel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-/* Jonathan Harrington - Completing the game*/
+/* Jonathan Harrington Adding Time Travel Next */
 
 import {useState} from 'react'
 
@@ -16,8 +16,16 @@ export default function Board() {
     const [xIsnext, setXIsnext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
-    function handleClick (i) {
-        if (squares[i]) {
+    const winner = calculateWinner(squares)
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next Player: " + (xIsnext ? "X" : "O");
+    }
+
+    function handleClick (i){
+        if (squares[i] || calculateWinner(squares)) {
             return;
         }
         const nextSquares = squares.slice();
@@ -35,6 +43,8 @@ export default function Board() {
     return (
         /* single lines needs ; while fragments do not */
         <>
+            <div className= "status">{status}</div>
+
             <div className="board-row">
                 <Square value = {squares[0]} onSquareClick={() => handleClick(0)} /> {/* 1 */}
                 <Square value = {squares[1]} onSquareClick={ () => handleClick(1)} /> {/* 2 */}
@@ -52,4 +62,23 @@ export default function Board() {
             </div>
         </>
     )
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 }
